79b08ec4b7b7c834e5334d1061ea79266f2e9e3d
B <b_easyx@protonmail.com>
2021-10-06 18:29:26 -0400

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NotFound = () => {
+  return(
+    <>
+      <h1>Page not found! <span role="img" aria-label="crying face">😢</span></h1>
+      <p>It looks like that page doesn't exist.</p>
+    </>
+  );
+}
+export default NotFound;
